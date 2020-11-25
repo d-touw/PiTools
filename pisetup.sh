2e43c135133672b8a2ae2fed69932ebf86d39432
d-touw <57910883+d-touw@users.noreply.github.com>
2020-11-25 13:54:58 +0100

    Init
    First pi tools

---
+++ pisetup.sh
@@ -0,0 +1,39 @@
+is_user_root()
+# function verified to work on Bash version 4.4.18
+# both as root and with sudo; and as a normal user
+{
+    ! (( ${EUID:-0} || $(id -u) ))
+}
+
+if is_user_root; then
+    echo 'You are the almighty root!'
+
+
+apt-get update
+apt-get upgrade
+apt-get install build-essential
+
+touch /etc/network/interfaces/eth01
+touch /etc/network/interfaces/eth02
+
+
+cat > /etc/network/interfaces/eth01 <<'EOL'
+auto eth0:1
+iface eth0:1 inet static
+        address 192.168.1.2
+        netmask 255.255.255.0
+EOL
+
+cat > /etc/network/interfaces/eth01 <<'EOL'
+auto eth0:2
+iface eth0:2 inet static
+        address 192.168.2.90
+        netmask 255.255.255.0
+EOL
+
+./APSetup.sh
+
+
+else
+    echo 'Please run this as Root/Sudo'
+fi
